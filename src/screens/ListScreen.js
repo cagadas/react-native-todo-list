b006e4687a2b09b54b21fa994ad49dca32dc7dd5
cagadas <cagadas11@gmail.com>
2022-04-15 16:34:55 +0300

    Navigation between screens

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { AntDesign } from '@expo/vector-icons';
 
-const ListScreen = () => {
+const ListScreen = ({ navigation }) => {
     return (
-        <View>
-            <Text>List</Text>
+        <View style={{ flex: 1 }}>
+            <TouchableOpacity onPress={() => navigation.navigate('Detail')}>
+                <Text>List</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.addButton} onPress={() => navigation.navigate('Add')}>
+                <AntDesign name="plus" size={24} color="white" />
+            </TouchableOpacity>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    addButton: {
+        height: 50,
+        width: 50,
+        borderRadius: 50,
+        bottom: 10,
+        position: 'absolute',
+        alignSelf: 'center',
+        backgroundColor: '#2e86de',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
 
 export default ListScreen
