ed0a6e138b70045aae8bb1e4439da6d3f1b214ff
cagadas <cagadas11@gmail.com>
2022-04-16 11:55:42 +0300

    Saving items to storage

--- src/context/ListContext.js
+++ src/context/ListContext.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import axios from '../util/axios';
 
 const ListContext = React.createContext();
@@ -7,28 +8,61 @@ export const ListProvider = ({ children }) => {
 
     const [itemList, setItemList] = useState([])
 
-    const getItemList = async () => {
-        const res = await axios.get("/simpsons");
-        setItemList(res.data)
+    const checkStore = () => {
+        return new Promise((resolve, reject) => {
+            AsyncStorage.getItem('list').then(res => {
+                if (res) {
+                    resolve(true)
+                } else {
+                    resolve(false)
+                }
+            }).catch(err => {
+                reject(err)
+            })
+        })
+
     }
 
-    const addItem = (item) => {
+    const getItemList = async () => {
+        let res = []
+        try {
+            if (await checkStore()) {
+                res = await AsyncStorage.getItem('list')
+                res = JSON.parse(res)
+            } else {
+                await axios.get("/simpsons").then(response => {
+                    res = response.data
+                })
+                await AsyncStorage.setItem('list', JSON.stringify(res))
+            }
+        } catch (err) {
+            console.log(err)
+        }
+        setItemList(res)
+    }
+
+    const addItem = async (item) => {
         const maxID = itemList.reduce((a, b) => {
             return (a.id > b.id) ? a.id : b.id
         })
-        setItemList([...itemList, { ...item, id: `${parseInt(maxID) + 1}` }]);
+        const newList = [...itemList, { ...item, id: `${parseInt(maxID) + 1}` }]
+        setItemList(newList);
+        await AsyncStorage.setItem('list', JSON.stringify(newList))
     }
 
-    const changeItemPosition = (index, d) => {
+    const changeItemPosition = async (index, d) => {
         const order = d === "up" && index !== 0 ? index - 1 :
             d === "down" && itemList.length > index ? index + 1 : index
         const item = itemList.splice(index, 1)[0];
         itemList.splice(order, 0, item)
-        setItemList([...itemList])
+        const changedList = itemList
+        await AsyncStorage.setItem('list', JSON.stringify(changedList))
+        setItemList([...changedList])
     }
 
-    const deleteItem = (id) => {
+    const deleteItem = async (id) => {
         const filterList = itemList.filter(t => t.id !== id)
+        await AsyncStorage.setItem('list', JSON.stringify(filterList))
         setItemList([...filterList])
     }
 
